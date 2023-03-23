5defcb4d8659b882f0f2cf0eb64e3223b53c259e
Ajinkyab2001 <ajinkyab2001@gmail.com>
2023-03-23 14:30:29 +0530

    font color change

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('namaste');
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = 'hello';
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// get element by id
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+console.log(headerTitle);
+// headerTitle.textcontent = 'hello';
+// headerTitle.innerText = 'goodbye';
+
+header.style.borderBottom = 'solid 3px #000';
+
+var addItems = document.getElementById('add');
+console.log(addItems);
+
+addItems.style.color = 'green';
+
+ 
